eca18d30f9fd21058f1a7ba57c2f4c155f265f76
Erick <moralerick90@gmail.com>
2026-07-24 16:31:11 -0700

    init selenium unit test struct

---
+++ main.py
@@ -0,0 +1,18 @@
+import unittest
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys   
+from selenium.webdriver.common.by import By 
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_search_in_python_org(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ -- "__main__":
+    unittest.main()
