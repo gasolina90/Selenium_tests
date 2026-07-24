4f1d9559c89dba4dc033499fe49e4fb4b8c46b0d
Erick <moralerick90@gmail.com>
2026-07-24 15:33:05 -0700

    added simple tests

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+from selenium import webdriver 
+# Provides all the webdriver implementations such as:
+# Firefox, Chrome, IE (Internet Explorer), Remote
+from selenium.webdriver.common.keys import Keys # Provides keys found in keyboard  
+from selenium.webdriver.common.by import By # Provides classes to locate elements 
+                                            # within a document
 
+# Create instance of Chrome Webdriver
+driver = webdriver.Chrome()
+# 
+driver.get("http://www.python.org")
+
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+
+assert "No results found." not in driver.page_source
+driver.close()
