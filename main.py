5a7cb0b13c7675e12fcf10a34af2ff2c101baa15
Erick <moralerick90@gmail.com>
2026-07-24 19:51:01 -0700

    added search logic, setup, and teardown

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import unittest
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys   
 from selenium.webdriver.common.by import By 
+from time import sleep
 
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.driver = webdriver.Chrome()
 
     def test_search_in_python_org(self):
-        pass
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element(By.NAME, "q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        self.assertNotIn("No results found", driver.page_source)
 
     def tearDown(self):
-        pass
+        self.driver.close()
+
+prs = PythonOrgSearch()
+
+prs.setUp()
+prs.test_search_in_python_org()
+prs.tearDown
 
 if __name__ -- "__main__":
-    unittest.main()
+    unittest.main(argv=["first-arg-is-ignored"], exit=False)
